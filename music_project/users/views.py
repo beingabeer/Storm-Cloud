f4d1a7ae735b1ea3d0c00369b0e0ffc7a5de06c7
Abeer <abeer.sewak@gmail.com>
2019-04-05 17:40:47 -0500

    Added an email field to the user registration form

--- music_project/users/views.py
+++ music_project/users/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Hi {username}! Your Account has been created. You can now log in.')
             return redirect('songs:index')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
